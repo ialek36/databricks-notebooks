02e31ab00471609476f249607957390d46dd8e06
ialek36 <ialek36@gmail.com>
2021-06-08 13:55:24 -0400

    initial commit

---
+++ notebooks/sql-sandbox.sql
@@ -0,0 +1,30 @@
+-- Databricks notebook source
+show databases;
+
+-- COMMAND ----------
+
+use mydb;
+show tables;
+
+-- COMMAND ----------
+
+select * from mydb.employee;
+
+-- COMMAND ----------
+
+select * from legsim.stocks limit 10
+
+-- COMMAND ----------
+
+select count(*) from mydb.sales_data4;
+
+-- COMMAND ----------
+
+select * from mydb.sales_data4 limit 20;
+
+-- COMMAND ----------
+
+select year, quarter, count(*) from mydb.sales_data4 group by year,quarter;
+
+-- COMMAND ----------
+
