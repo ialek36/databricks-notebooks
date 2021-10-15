bc9f47b3d317c7ce218b14ba0e5690fef1919c3e
Alekseev <ialek@909c4acf77aa.ant.amazon.com>
2021-10-15 13:14:05 -0400

    added apikey parms

--- test_etl.py
+++ test_etl.py
@@ -1,3 +1,12 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--apikey',  help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args)
+
 print("started test...")
+print(args.apikey)
 
 print("finished test...")
