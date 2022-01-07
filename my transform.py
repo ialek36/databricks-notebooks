1ecd8a18d0949fa41124f7b4faf445c3fd14ffe6
ialek36 <ialek36@gmail.com>
2022-01-07 16:49:36 -0500

    Delete my transform.py
    deleted

--- my transform.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from pyspark.sql import SparkSession
-
-
-@pytest.fixture(scope="session")
-def spark_session():
-    spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
-    return spark
